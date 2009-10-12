4b8a57053dc7caff39d93cbe1b40e143a792bee8
Petyo Ivanov <underlog@gmail.com>
2009-10-12 15:35:22 +0300

    Specs

--- spec/shellshot_spec.rb
+++ spec/shellshot_spec.rb
@@ -1,7 +1,32 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Shellshot" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+require File.join(File.dirname(__FILE__), %w[spec_helper])
+
+describe Shellshot do
+
+  it "should execute the given command" do
+    Shellshot.exec("touch file")
+    "file".should be_a_file
   end
+
+  it "should pipe the out to the right file" do 
+    Shellshot.exec(%q[ruby -e 'puts "Hello World"'], :stdout => "file")
+    "file".should contain("Hello World")
+  end
+
+  it "should pipe the err to the right file" do 
+    Shellshot.exec(%q[ruby -e '$stderr << "error"'], :stderr => "file")
+    "file".should contain("error")
+  end
+
+  it "should pipe everything to the right file" do
+    Shellshot.exec(%q[ruby -e '$stderr << "Hello "; puts "World"'], :stdall => "file")
+    "file".should contain("Hello World")
+  end
+
+  it "should cancel execution on time if timeout provided" do
+    lambda { Shellshot.exec %q[ruby -e 'sleep 10000'], :timeout => 1 }.should raise_error(Timeout::Error)
+  end
+
 end
+
+# EOF
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,43 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'shellshot'
-require 'spec'
-require 'spec/autorun'
+
+require File.expand_path(
+    File.join(File.dirname(__FILE__), %w[.. lib shellshot]))
+
+require 'tmpdir'
+require 'rubygems'
+require 'ruby-debug'
+
+Spec::Matchers.define :be_a_file do 
+  match do |path|
+    File.exists?(path)
+  end
+end
+
+Spec::Matchers.define :contain do |contents|
+  match do |path|
+    File.exists?(path) && File.read(path).strip == contents
+  end
+
+  failure_message_for_should do |path|
+    if File.exists?(path)
+      "expected #{path} to contain '#{contents}', got '#{File.read(path).strip}'"
+    else
+      "expected #{path} to contain '#{contents}', but the file does not exist."
+    end
+  end
+end
 
 Spec::Runner.configure do |config|
-  
+  config.before(:each) do
+    @tmpdir = Dir.mktmpdir 
+    Dir.chdir(@tmpdir)
+  end
+
+  config.after(:each) do
+    FileUtils.remove_entry_secure @tmpdir
+  end
+
 end
+
+
+
+# EOF
